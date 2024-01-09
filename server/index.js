bba44dee2f818145cdfd8889cda8940442f47575
Jordan Williams <jordanw0412@gmail.com>
2024-01-09 07:47:45 -0500

    added database from Capstone Project and appropriate resolvers

--- server/index.js
+++ server/index.js
@@ -4,11 +4,30 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // types
 import { typeDefs } from './schemas.js';
 
+const monoDataFetch = await fetch("https://capstone-project-1cyy.vercel.app/monostats")
+const monoData = await monoDataFetch.json()
+
+const usersFetch = await fetch("https://capstone-project-1cyy.vercel.app")
+const users = await usersFetch.json()
+
+const resolvers = {
+    Query: {
+        users(){
+            return users
+        },
+
+        monoData() {
+            return monoData
+        }
+
+    }
+}
 
 const server = new ApolloServer({
   typeDefs,
-//   resolvers,
+  resolvers
 });
 
 const { url } = await startStandaloneServer(server);
+console.log(monoData)
 console.log(`🚀 Server ready at ${url}`);
